681a3c80864c719e78a4d6195b5e468a8c63a4b6
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2023-08-05 15:39:57 -0300

    feat: creat Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './style.scss';
+import Image from 'next/image';
+// import Logo from './logo.svg';
 
 export default function Header() {
   return (
-    <header>
-      header
+    <header id="header">
+      <div className="logo-container">
+        <Image src="/logo.svg" alt="Logo Tech in Lar" fill />
+      </div>
+      <div className="btns-container">
+        <button type="button" className="contact-btn">
+          Contato
+        </button>
+        <button type="button" className="menu-btn">
+          <div className="menu-btn__icon" />
+        </button>
+      </div>
     </header>
   );
 }
