e632e3b1fe45f6c002b0b20b788acb23814e0e7c
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2023-08-17 18:38:38 -0300

    feat: Created and styling Phrase section

--- src/Sections/Phrase/index.tsx
+++ src/Sections/Phrase/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './style.scss';
 
 export default function Phrase() {
   return (
-    <section>
-      Phrase
+    <section className="phrase">
+      <div>
+        <blockquote>
+          A tecnologia move o mundo e facilita nossas vidas em tantos aspectos.
+          Abraçar a inovação é o caminho para o progresso.
+        </blockquote>
+        <div className="phrase__author">
+          <span>Jeff Bezos</span>
+          <span>Fundador da Amazon</span>
+        </div>
+      </div>
     </section>
   );
 }
